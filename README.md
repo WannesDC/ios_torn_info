ec296ade5c79768f1484b3afacccc50325a1bdb8
Wannes De Craene <wannesdecraene@hotmail.com>
2020-08-15 14:19:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Torn Info iOS
---
+++ Torn InfoTests/Torn_InfoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Torn_InfoTests.swift
+//  Torn InfoTests
+//
+//  Created by Wannes De Craene on 15/08/2020.
+//  Copyright © 2020 Wannes De Craene. All rights reserved.
+//
+
+import XCTest
+@testable import Torn_Info
+
+class Torn_InfoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
